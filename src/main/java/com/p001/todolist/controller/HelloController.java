f0379c69ea528f136cc8ab0f223fa2278f411c14
chandra <chandrasekhar@bijib.in>
2025-04-28 14:47:38 +0530

    setUp

---
+++ src/main/java/com/p001/todolist/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.p001.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Chandra! Your GET API is working 🚀";
+    }
+}
